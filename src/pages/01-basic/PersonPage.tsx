02c3e73e5f27c8401f6910ed512aa7215a260796
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-25 12:25:02 -0400

    feat(dev): :tada: start the project
    fist step to learn zustand, let to it

--- src/pages/01-basic/PersonPage.tsx
+++ src/pages/01-basic/PersonPage.tsx
@@ -1,57 +1,55 @@
 import { WhiteCard } from '../../components';
 
-
-
 export const PersonPage = () => {
   return (
     <>
       <h1>Persona</h1>
-      <p>Información que se compartirá a otro store, Session Storage y Firebase</p>
+      <p>
+        Información que se compartirá a otro store, Session Storage y Firebase
+      </p>
       <hr />
 
-      <WhiteCard className="flex items-center justify-center p-12">
-        <div className="mx-auto w-full max-w-[550px]">
+      <WhiteCard className='flex items-center justify-center p-12'>
+        <div className='mx-auto w-full max-w-[550px]'>
           <form>
-            <div className="-mx-3 flex flex-wrap">
-              <div className="w-full px-3 sm:w-1/2">
-                <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
+            <div className='-mx-3 flex flex-wrap'>
+              <div className='w-full px-3 sm:w-1/2'>
+                <div className='mb-5'>
+                  <label className='mb-3 block text-base font-medium text-[#07074D]'>
                     Primer Nombre
                   </label>
                   <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    placeholder="Primer Nombre"
+                    type='text'
+                    name='firstName'
+                    id='firstName'
+                    placeholder='Primer Nombre'
                   />
                 </div>
               </div>
-              <div className="w-full px-3 sm:w-1/2">
-                <div className="mb-5">
-                  <label
-                    className="mb-3 block text-base font-medium text-[#07074D]"
-                  >
+              <div className='w-full px-3 sm:w-1/2'>
+                <div className='mb-5'>
+                  <label className='mb-3 block text-base font-medium text-[#07074D]'>
                     Apellido
                   </label>
                   <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    placeholder="Apellido"
+                    type='text'
+                    name='lastName'
+                    id='lastName'
+                    placeholder='Apellido'
                   />
                 </div>
               </div>
             </div>
-  
-            <pre className="bg-gray-200 p-5 rounded-[20px]">
-              {
-                JSON.stringify({
+
+            <pre className='bg-gray-200 p-5 rounded-[20px]'>
+              {JSON.stringify(
+                {
                   firstName: '',
-                  lastName: ''
-                }, null, 2)
-              }
+                  lastName: '',
+                },
+                null,
+                2
+              )}
             </pre>
           </form>
         </div>
--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -7,20 +7,13 @@ export const JiraPage = () => {
       <p>Manejo de estado con objectos de Zustand</p>
       <hr />
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          
-          <JiraTasks title='Pendientes' value='pending' />
-          
-          <JiraTasks title='Avanzando' value='in-progress' />
-          
-          <JiraTasks title='Terminadas' value='done' />
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <JiraTasks title='Pendientes' value='pending' />
 
+        <JiraTasks title='Avanzando' value='in-progress' />
+
+        <JiraTasks title='Terminadas' value='done' />
       </div>
-
-      
-
-
-
     </>
   );
 };
