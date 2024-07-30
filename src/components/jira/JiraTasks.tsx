c19c88b217011b7f399da30e1312b13076559609
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-29 20:09:15 -0400

    feat(dev): :sparkles: drag and drop events

--- src/components/jira/JiraTasks.tsx
+++ src/components/jira/JiraTasks.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames';
 import {
   IoCheckmarkCircleOutline,
   IoEllipsisHorizontalOutline,
@@ -5,6 +6,7 @@ import {
 
 import { Task, TaskStatus } from '../../../interfaces';
 import { SingleTask } from './SingleTask';
+import { useTaskStore } from '../../store/tasks';
 
 interface Props {
   title: string;
@@ -12,9 +14,32 @@ interface Props {
   tasks: Task[];
 }
 
-export const JiraTasks = ({ title, tasks }: Props) => {
+export const JiraTasks = ({ value, title, tasks }: Props) => {
+  const isDragging = useTaskStore((store) => !!store.draggingTaskId);
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log('handleDragOver: ', value);
+  };
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log('handleDragLeave: ', value);
+  };
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log('handleDrop: ', value);
+  };
+
   return (
-    <div className='!text-black relative flex flex-col rounded-[20px]  bg-white bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px]'>
+    <div
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      className={classnames(
+        `!text-black border-4  relative flex flex-col rounded-[20px]  bg-white bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px]`,
+        { 'border-blue-500 border-dotted': isDragging }
+      )}
+    >
       {/* Task Header */}
       <div className='relative flex flex-row justify-between'>
         <div className='flex items-center justify-center'>
--- src/store/tasks/task.store.ts
+++ src/store/tasks/task.store.ts
@@ -1,14 +1,19 @@
 import { create, StateCreator } from 'zustand';
 import { Task, type TaskStatus } from '../../../interfaces/';
+import { devtools } from 'zustand/middleware';
 
 interface TaskState {
   // tasks: { [key: string]: Task };
   tasks: Record<string, Task>;
+  draggingTaskId?: string;
 
   getTaskByStatus: (status: TaskStatus) => Task[];
+  setDraggingTaskId: (taskId: string) => void;
+  removeDraggingTaskId: () => void;
 }
 
-const storeApi: StateCreator<TaskState> = () => ({
+const storeApi: StateCreator<TaskState> = (set, get) => ({
+  draggingTaskId: undefined,
   tasks: {
     'Ach 1': { id: 'Ach 1', title: 'task 1', status: 'done' },
     'Ach 2': { id: 'Ach 2', title: 'task 2', status: 'in-progress' },
@@ -16,9 +21,22 @@ const storeApi: StateCreator<TaskState> = () => ({
     'Ach 4': { id: 'Ach 4', title: 'task 4', status: 'done' },
   },
 
-  getTaskByStatus(status:TaskStatus): Task[] {
-    return Object.values(this.tasks).filter((task) => task.status === status);
+  getTaskByStatus: (status: TaskStatus): Task[] => {
+    const tasks = get().tasks;
+    return Object.values(tasks).filter(
+      (task) => task.status === status
+    ) as Task[];
+  },
+
+  setDraggingTaskId: (taskId: string) => {
+    set({ draggingTaskId: taskId });
+  },
+
+  removeDraggingTaskId: () => {
+    set({ draggingTaskId: undefined });
   },
 });
 
-export const useTaskStore = create<TaskState>()(storeApi);
+export const useTaskStore = create<TaskState>()(
+  devtools(storeApi, { name: 'tasks' })
+);
